525a98e364bafae537b2d2e560be128845d8fbc9
shivam361v <shivam361v@gmail.com>
2023-07-19 15:31:46 +0530

    fourth commit

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require('./db')
+const cors = require('cors')
 
 //importing database connection
 mongoDB();
